234bf1ab9154e823a9976b8e87b759c432860d99
KnoxNeon <iftey10@gmail.com>
2025-09-11 19:55:25 +0600

    add history

--- index.js
+++ index.js
@@ -7,28 +7,45 @@ const increaseHeart = () =>{
     heartValueUpdate.innerHTML = heartValueConverted;
 }
 
-const decreaseCoin = () =>{
-    let coinValue = document.getElementById("coin-value").innerHTML
+const decreaseCoin = (serviceName, serviceNumber) =>{
+    let coinValue = document.getElementById("coin-value").innerHTML;
     let coinValueConverted = parseInt(coinValue);
 
     if(coinValueConverted == 0){
-        alert("You do not have sufficient coins")
+        alert("❌ You do not have sufficient coins! You need at least 20 coins to call.")
     }
     else{
-        let coinValue = document.getElementById("coin-value").innerHTML
+        let coinValue = document.getElementById("coin-value").innerHTML;
         let coinValueConverted = parseInt(coinValue);
         coinValueConverted = coinValueConverted - 20;
+
+        alert(`📞 Calling ${serviceName} ${serviceNumber}`);
         let coinValueUpdate = document.getElementById("coin-value");
         coinValueUpdate.innerHTML = coinValueConverted;
-    }
-    
+
+        const history = document.getElementById("add-history");
+        const historyUpdate = document.createElement("div")
+        historyUpdate.innerHTML =`
+        <div class="bg-[#FAFAFA] rounded-xl p-4 mb-4 flex justify-between items-center">
+        <div>
+            <p class="font-semibold text-lg">${serviceName}</p>
+            <h3 class="font-normal text-lg text-[#5C5C5C]">${serviceNumber}</h3>
+        </div>
+        <div>${new Date().toLocaleTimeString()}</div>
+        </div>
+        `
+        history.appendChild(historyUpdate);
+    }    
 }   
 
-const increaseCopy = () =>{
+const increaseCopy = (serviceNumber) =>{
     let copyValue = document.getElementById("copy-value").innerHTML
     let copyValueConverted = parseInt(copyValue);
     copyValueConverted = copyValueConverted + 1;
 
+    navigator.clipboard.writeText(serviceNumber)
+    .then(alert(`Copied Number: ${serviceNumber}`));
+    
     let copyValueUpdate = document.getElementById("copy-value");
     copyValueUpdate.innerHTML = copyValueConverted;
 }   
